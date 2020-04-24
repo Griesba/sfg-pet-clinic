f04d2406298069fa3316e5b303a817778fede28f
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-04-25 01:36:21 +0200

    Upgrade to Junit 5 #1

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ pet clinic web app
 # dependency injection vs IoC
   dependency injection is class composition. You compose your classes with depency injection in mind.
   IoC is the runtime environment of your code. ie Springframework's IoC container. Spring is in control of the injection of dependency.
+
+
+#Unit test
+we are using Junit 5 in this project. In order to do that Junit 4 (Junit-vintage) was excluded
+manually in the spring-boot-starter-test dependency. 
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,26 +1,15 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest (classes = SfgPetClinicApplication.class)
-@ContextConfiguration
 public class SfgPetClinicApplicationTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
-    public void tes123 () {
-        assertTrue(true);
+    public void contextLoad () {
+
     }
 }
